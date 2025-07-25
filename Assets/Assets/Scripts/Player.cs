0e1b30786b6caacbadf6867bf9ddd47515a7596c
PaladinWolf2675 <timberwolf2675@gmail.com>
2025-07-24 23:23:24 -0400

    Finished creating character movement script.

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector3 playerDirection;
-
-    public float gravity = -9.8f;
-
-    public float strength = 5f;
+    
+    public float gravity = -9.8f;// How fast the bird will fall
+    
+    public float strength = 5f;// How high the bird will fly
     
 
     private void Update()
     {
         // This will check for key presses on a keyboard
-        if (Input.GetKeyDown(KeyCode.Space)  || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             playerDirection = Vector3.up * strength;
         }
@@ -21,6 +21,14 @@ public class Player : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                playerDirection = Vector3.up * strength;
+            }
         }
+
+        playerDirection.y += gravity * Time.deltaTime;
+        transform.position += playerDirection * Time.deltaTime;
     }
 }
