7f1585b905cad4a41906f9d9748bdd33a2583261
PaladinWolf2675 <timberwolf2675@gmail.com>
2025-08-10 19:49:31 -0400

    Added in a modified version of the player script. The modified script contains comments that anyone who does not know C sharp code should still be able to understand what is written.

--- Assets/unity-flappy-bird-tutorial-main/unity-flappy-bird-tutorial-main/Assets/Scripts/Player.cs
+++ Assets/unity-flappy-bird-tutorial-main/unity-flappy-bird-tutorial-main/Assets/Scripts/Player.cs
@@ -2,69 +2,32 @@
 
 public class Player : MonoBehaviour
 {
-    public Sprite[] sprites;
-    public float strength = 5f;
-    public float gravity = -9.81f;
-    public float tilt = 5f;
-
-    private SpriteRenderer spriteRenderer;
-    private Vector3 direction;
-    private int spriteIndex;
-
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void Start()
-    {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
-    }
-
-    private void OnEnable()
-    {
-        Vector3 position = transform.position;
-        position.y = 0f;
-        transform.position = position;
-        direction = Vector3.zero;
-    }
+    private Vector3 playerDirection;
+    public float gravity = -9.8f;
+    public float flapStrenght = 5f;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-            direction = Vector3.up * strength;
+        //this will check for either a press of the spacebar or a click of the left mouse button
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            /*this section will move the player upwards based on the set value of the game engine
+             multiplied by the set value of the flapStrenght variable*/
+            playerDirection = Vector3.up * flapStrenght;
         }
+        //this will check for touch input on mobile devices
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
 
-        // Apply gravity and update the position
-        direction.y += gravity * Time.deltaTime;
-        transform.position += direction * Time.deltaTime;
-
-        // Tilt the bird based on the direction
-        Vector3 rotation = transform.eulerAngles;
-        rotation.z = direction.y * tilt;
-        transform.eulerAngles = rotation;
+            if (touch.phase == TouchPhase.Began)
+            {
+                playerDirection = Vector3.up * flapStrenght;
+            }
+        }
+        /*this will make the game frame rate independant 
+         so as not to cause issues with lower powered systems*/
+        playerDirection.y += gravity * Time.deltaTime;
+        transform.position += playerDirection * Time.deltaTime;
     }
-
-    private void AnimateSprite()
-    {
-        spriteIndex++;
-
-        if (spriteIndex >= sprites.Length) {
-            spriteIndex = 0;
-        }
-
-        if (spriteIndex < sprites.Length && spriteIndex >= 0) {
-            spriteRenderer.sprite = sprites[spriteIndex];
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Obstacle")) {
-            GameManager.Instance.GameOver();
-        } else if (other.gameObject.CompareTag("Scoring")) {
-            GameManager.Instance.IncreaseScore();
-        }
-    }
-
 }
